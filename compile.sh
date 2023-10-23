ded28956925e0738ff14ce65d9dc21a357a38116
Eric Stofferahn <Eric.Stofferahn@noaa.gov>
2023-10-23 15:11:00 -0400

    Adaptations to paths and yml to get a functioning MDTF environment.

--- compile.sh
+++ compile.sh
@@ -5,4 +5,6 @@
 ##new compilation
 #./src/conda/conda_env_setup.sh -e base --conda_root /lustre/f2/dev/Zun.Yin/anaconda --env_dir /lustre/f2/dev/Zun.Yin/anaconda/envs
 #./src/conda/conda_env_setup.sh -e python3_base --conda_root /lustre/f2/dev/Zun.Yin/anaconda --env_dir /lustre/f2/dev/Zun.Yin/anaconda/envs
-./src/conda/conda_env_setup.sh -e R_base --conda_root /home/zyin/miniconda3 --env_dir /home/zyin/miniconda3/envs
+./src/conda/conda_env_setup.sh -e R_base --env_dir /nbhome/Eric.Stofferahn/.conda/envs
+# It is still unclear to me why the generated mdtf file includes the wrong envs path, this fixes it
+sed -i 's|envs/_MDTF_base|envs/_MDTF_R_base|' mdtf
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 ./mdtf -f src/default_LoCo.jsonc
-#./mdtf -f src/default_tests.jsonc -v
