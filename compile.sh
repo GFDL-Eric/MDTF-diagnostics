773e6d2bec4c59352612731e2a58ed425d9771fd
Eric Stofferahn <Eric.Stofferahn@noaa.gov>
2023-11-09 14:21:10 -0500

    Removed hardcoded references to specific user.

--- compile.sh
+++ compile.sh
@@ -3,8 +3,9 @@
 #./src/conda/conda_env_setup.sh --all --conda_root /home/zyin/miniconda3
 
 ##new compilation
+sed -i 's/<USER>/'$USER'/' src/default_LoCo.jsonc
 #./src/conda/conda_env_setup.sh -e base --conda_root /lustre/f2/dev/Zun.Yin/anaconda --env_dir /lustre/f2/dev/Zun.Yin/anaconda/envs
 #./src/conda/conda_env_setup.sh -e python3_base --conda_root /lustre/f2/dev/Zun.Yin/anaconda --env_dir /lustre/f2/dev/Zun.Yin/anaconda/envs
-./src/conda/conda_env_setup.sh -e R_base --env_dir /nbhome/Eric.Stofferahn/.conda/envs
+./src/conda/conda_env_setup.sh -e R_base --env_dir /nbhome/$USER/.conda/envs
 # It is still unclear to me why the generated mdtf file includes the wrong envs path, this fixes it
 sed -i 's|envs/_MDTF_base|envs/_MDTF_R_base|' mdtf
